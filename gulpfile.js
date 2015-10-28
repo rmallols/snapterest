f293dac42775a5135af6a007a573e45e6e1b6e5b
rmallols <ricardo.mallols@gmail.com>
2015-10-28 00:08:03 +0000

    Chapter 2: Cleaning up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,11 @@ var gulp = require('gulp'),
     watch = require('gulp-watch');
 
 gulp.task('build', function () {
-   console.log('I am about to learn the essentials of React.js https://apps.twitter.com/app/8995886');
    return browserify('./source/app.js')
        .transform(babelify)
        .bundle()
        .pipe(source('snapterest.js'))
-       .pipe(gulp.dest('./build/'))
+       .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('watch', function() {
