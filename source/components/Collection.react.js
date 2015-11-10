7674f2d22d92c01053690394cc37d9d31b0867bd
rmallols <ricardo.mallols@gmail.com>
2015-11-10 00:11:59 +0000

    Chapter 6: builds Collection, Header and Tweet components

---
+++ source/components/Collection.react.js
@@ -0,0 +1,39 @@
+var React = Require('react'),
+    ReactDOMServer = require('react-dom/server'),
+    CollectionControls = require('./CollectionControls.react'),
+    TweetList = require('./TweetList.react'),
+    Header = require('./Header.react'),
+    Collection = React.createClass({
+        createHtmlMarkupStringOfTweetList: function () {
+            var htmlString = ReactDOMServer.renderToStaticMarkup(<TweetList tweets={this.props.tweets} />),
+                htmlMarkup = {
+                    html: htmlString
+                };
+            return JSON.stringify(htmlMarkup);
+        },
+        getListOfTweetIds: function () {
+            return Object.keys(this.props.tweets);
+        },
+        getNumberOfTweetsInCollection: function () {
+            return this.getListOfTweetIds().length;
+        },
+        render: function () {
+            var numberOfTweetsInCollection = this.getNumberOfTweetsInCollection(),
+                htmlMarkup;
+            if(numberOfTweetsInCollection > 0) {
+                htmlMarkup = this.createHtmlMarkupStringOfTweetList();
+                return  <div>
+                            <CollectionControls
+                                numberOfTweetsInCollection={numberOfTweetsInCollection}
+                                htmlMarkup={htmlMarkup}
+                                onRemoveAllTweetsFromCollection={this.props.onRemoveAllTweetsFromCollection}/>
+                            <TweetList
+                                tweets={this.props.tweets}
+                                onRemoveTweetFromCollection="{this.props.onRemoveTweetFromCollection}"/>
+                        </div>;
+            }
+            return <Header text="Your collection is empty" />;
+        }
+    });
+
+module.exports = Collection;
---
+++ source/components/Tweet.react.js
@@ -0,0 +1,43 @@
+var React = require('react'),
+    tweetStyle = {
+        position: 'relative',
+        display: 'inline-block',
+        width: '300px',
+        height: '400px',
+        margin: '10px'
+    },
+    imageStyle = {
+        maxHeight: '400px',
+        boxShadow: '0px 1px 1px 0px #aaa',
+        border: '1px solid #fff'
+    },
+    Tweet = React.createClass({
+        propTypes: {
+            tweet: function (properties, propertyName, componentName) {
+                var tweet = properties[propertyName];
+                if(!tweet) {
+                    return new Error('Tweet must be set');
+                }
+                if(!tweet.media) {
+                    return new Error('Tweet must have an image');
+                }
+            },
+            onImageClick: React.propTypes.func
+        },
+        handleImageClick: function () {
+            var tweet = this.props.tweet,
+                onImageClick = this.props.onImageClick;
+            if(onImageClick) {
+                onImageClick(tweet);
+            }
+        },
+        render: function () {
+            var tweet = this.props.tweet,
+                tweetMediaUrl = tweet.media[0].url;
+            return  <div style="{tweetStyle}">
+                        <img src="{tweetMediaUrl}" onClick="{this.handleImageClick}" style="{imageStyle}" />
+                    </div>;
+        }
+    });
+
+module.exports = Tweet;
